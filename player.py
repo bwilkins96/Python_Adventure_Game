7c40a770b78a3e18aa5d8fe6adb7cd150730c20d
bwilkins96 <ben.jr.speed@gmail.com>
2023-03-06 10:38:23 -0500

    player class commenting

--- player.py
+++ player.py
@@ -1,7 +1,14 @@
 # Player class for the Adventure Game project
 
 class Player:
+    'A player class designed for the Adventure Game project'
+
     def __init__(self, name):
+        """
+        Produces a player instance that stores name, amount of energy,
+        pages, whether a revive was used, and encountered enemies
+        e.g. Player(name)
+        """
         self.name = name.title()
         self.energy = 100
         self.pages = 0
@@ -9,50 +16,63 @@ class Player:
         self.encounters = {}
     
     def get_name(self):
+        "Returns the player's name"
         return self.name
     
     def get_energy(self):
+        "Returns the player's energy level"
         return self.energy
 
     def get_trinket_count(self):
+        "Returns the player's number of pages"
         return self.pages
     
     def has_used_chance(self):
+        "Returns whether the player has used a revive"
         return self.used_second_chance
     
     def reduce_energy(self, amt):
+        "Reduces player energy by amount"
         self.energy -= amt
 
         if self.energy < 0:
             self.energy = 0
 
     def add_energy(self, amt):
+        "Increases player energy by amount"
         self.energy += amt
 
         if self.energy > 100:
             self.energy = 100
 
     def add_trinket(self, amt):
+        "Adds amount to player page count"
         self.pages += amt
     
     def reduce_trinket(self, amt):
+        "Subtracts amount from player page count"
         self.pages -= amt
 
         if self.pages < 0:
             self.pages = 0
     
     def add_encounter(self, name):
+        "Adds to the tracking of player encounters"
         encounter = self.encounters.get(name, 0)
         self.encounters[name] = encounter + 1
     
     def print_status(self):
+        "Prints the player's current energy and page count"
         print(f'\n{self.get_name()} | Energy: {self.get_energy()}, Pages: {self.get_trinket_count()}\n')
     
     def look_behind(self, moves):
+        "Prints a message describing the player looking behind themselves"
+
         print('\nYou look behind you and see the crashing waves of the ocean,')
         print(f'as well as the ship you came here on, the Ship of {self.get_name()}!')
         print('If you replaced every piece of your ship over time, would it be the same ship?\n')
 
+        # Prints a message related to previous moves
         moves_taken = 10 - moves
         if moves_taken == 1:
             print('You also see your footprints from the previous move.\n')
@@ -60,6 +80,7 @@ class Player:
             print(f'You also see your footprints from the previous {moves_taken} moves.\n')
 
     def attack_enemy(self, enemy, choice):
+        "Handles player battle choice"
         if choice == 'a':
             enemy.handle_attack()
         elif choice == 'd':
@@ -68,8 +89,10 @@ class Player:
             enemy.handle_reassurance()
     
     def handle_defeat(self):
+        "Handles player energy reaching zero and player revive"
         print('Your energy has been reduced to zero!')
 
+        # Allows revive if not already used and page count is at least 5
         if (not self.has_used_chance()) and (self.get_trinket_count() >= 5):
             input('\nPress <enter> to continue. ')
             print('\nYou are laying in the sand when you have an idea!')
@@ -92,6 +115,7 @@ class Player:
         input('\nPress <enter> to continue. ')
 
     def print_end_personal(self):
+        "Prints ending player energy and page count"
         energy = self.get_energy()
         pages = self.get_trinket_count()
 
@@ -108,11 +132,13 @@ class Player:
         print(f'At the end of your adventure, you had {energy} energy and {pages}.')
     
     def print_encounters(self):
+        "Prints information about encountered enemies"
         total_encounters = len(self.encounters.keys())
         encounter_str = '\nYou encountered '
 
         count = 1
         for enemy in self.encounters:
+            # Adds to and formats encounter string 
             num_encountered = self.encounters[enemy]
 
             enemy_name = enemy.lower()
@@ -122,6 +148,7 @@ class Player:
             if count == total_encounters and count > 1: 
                 encounter_str += 'and '
 
+            # Handles making enemy names singular or plural
             if num_encountered == 1:
                 encounter_str += f'1 {enemy_name}'
             else:
@@ -137,17 +164,21 @@ class Player:
 
             count += 1
         
+        # Final formatting of encounter string
         if total_encounters == 0:
             encounter_str += 'no enemies'
         elif total_encounters <= 2:
+            # Removes extra space
             encounter_str = encounter_str[:len(encounter_str)-1]
         else:
+            # Removes comma and extra space
             encounter_str = encounter_str[:len(encounter_str)-2]
         
         encounter_str += '.'
         print(encounter_str)
     
     def print_end_stats(self, treasure_found):
+        "Prints player's ending stats"
         self.print_end_personal()
         self.print_encounters()
 
